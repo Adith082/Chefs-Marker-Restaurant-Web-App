17d2ba509fb94763607958083787e52a2fd78d8c
Adith082 <adibhasan35@gmail.com>
2026-03-21 21:15:53 +0600

    initial-commit

---
+++ Backend/src/main/java/com/deepseeker/ChefsMarker/ChefsMarkerApplication.java
@@ -0,0 +1,13 @@
+package com.deepseeker.ChefsMarker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChefsMarkerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChefsMarkerApplication.class, args);
+	}
+
+}
---
+++ Backend/src/test/java/com/deepseeker/ChefsMarker/ChefsMarkerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.deepseeker.ChefsMarker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChefsMarkerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
